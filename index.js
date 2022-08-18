910c4b1ec7509c9e0acc8ffd9b859eb3f624d184
gusgluna <gusgluna@gmail.com>
2022-08-18 12:42:17 -0500

    Changing post method

--- index.js
+++ index.js
@@ -38,67 +38,59 @@ db.once("open", () => console.log("Connected to Database"));
 const Url = require("./models/url");
 
 // Post Endpoint
-app.post(
-  "/api/shorturl",
-  //Middleware to validate Url
-  (req, res, next) => {
-    //Validate the url
-    try {
-      new URL(req.body.url);
-    } catch (error) {
-      return res.json({ error: "Invalid URL" });
-    }
-    //Validate Host
-    const url = new URL(req.body.url);
-    dns.lookup(url.hostname, (error, addres, family) => {
-      if (error?.code == "ENOTFOUND") {
-        return res.json({ error: "Invalid Hostname" });
-      }
-      // Validate if already exist de url on DB
-      Url.findOne({ original_url: url.origin }).exec((error, result) => {
-        if (result?.original_url != undefined) {
-          return res.json({
-            original_url: result.original_url,
-            short_url: result.short_url,
-          });
-        }
-        next();
-      });
-    });
-  },
-  (req, res) => {
-    const url = new URL(req.body.url);
-    let shortId;
-    // Consult the DB to find the last url_short id
-    Url.findOne({})
-      .sort({ short_url: "desc" })
-      .exec((error, result) => {
-        if (error) {
-          return res.json({ error_mesagge: error.message });
-        }
-        if (result == undefined) {
-          shortId = 1;
-        } else {
-          shortId = result.short_url + 1;
-        }
-        // Create new document
-        const newUrl = new Url({
-          original_url: url.origin,
-          short_url: shortId,
-        });
-        // Insert new document
-        newUrl.save((error, data) => {
-          if (error) {
-            res.json({ error_mesagge: error.message });
-          }
-          return res.json({
-            original_url: data.original_url,
-            short_url: data.short_url,
-          });
-        });
-      });
+app.post("/api/shorturl", (req, res) => {
+  //Validate the url
+  try {
+    new URL(req.body.url);
+  } catch (error) {
+    return res.json({ error: "Invalid URL" });
   }
-);
+  //Validate Host
+  const url = new URL(req.body.url);
+  dns.lookup(url.hostname, (error, addres, family) => {
+    if (error?.code == "ENOTFOUND") {
+      return res.json({ error: "Invalid Hostname" });
+    }
+    // Validate if already exist de url on DB
+    Url.findOne({ original_url: url.origin }).exec((error, result) => {
+      if (result?.original_url != undefined) {
+        return res.json({
+          original_url: result.original_url,
+          short_url: result.short_url,
+        });
+      }
+      let shortId;
+      // Consult the DB to find the last url_short id
+      Url.findOne({})
+        .sort({ short_url: "desc" })
+        .exec((error, result) => {
+          if (error) {
+            return res.json({ error_mesagge: error.message });
+          }
+          if (result == undefined) {
+            shortId = 1;
+          } else {
+            shortId = result.short_url + 1;
+          }
+          // Create new document
+          const newUrl = new Url({
+            original_url: url.origin,
+            short_url: shortId,
+          });
+          // Insert new document
+          newUrl.save((error, data) => {
+            if (error) {
+              res.json({ error_mesagge: error.message });
+            }
+            return res.json({
+              original_url: `${data.original_url}`,
+              short_url: data.short_url,
+            });
+          });
+        });
+    });
+  });
+});
 
 app.get("/api/shorturl/:id", (req, res) => {
   const id = req.params.id;
